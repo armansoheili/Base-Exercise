30cd1e2a9a35db2031abd34653d3bfc06dc3d6fa
armansoheili <113059160+armansoheili@users.noreply.github.com>
2025-01-19 08:27:30 +1200

    Update AddressBook.sol

--- The new keyword/AddressBook.sol
+++ The new keyword/AddressBook.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract AddressBook {
-    address public owner;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract AddressBook is Ownable {
     struct Contact {
         uint id;
         string firstName;
@@ -12,68 +12,51 @@ contract AddressBook {
     }
 
     mapping(uint => Contact) private contacts;
-    uint[] private contactIds;
+    uint private nextId = 1;
 
     error ContactNotFound(uint id);
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Caller is not the owner");
-        _;
-    }
-
-    constructor(address _owner) {
-        owner = _owner;
-    }
+    constructor() Ownable() {}
 
     function addContact(
-        uint _id,
         string memory _firstName,
         string memory _lastName,
         uint[] memory _phoneNumbers
     ) public onlyOwner {
-        contacts[_id] = Contact(_id, _firstName, _lastName, _phoneNumbers);
-        contactIds.push(_id);
+        Contact memory newContact = Contact(nextId, _firstName, _lastName, _phoneNumbers);
+        contacts[nextId] = newContact;
+        nextId++;
     }
 
     function deleteContact(uint _id) public onlyOwner {
         if (contacts[_id].id == 0) {
             revert ContactNotFound(_id);
         }
-
         delete contacts[_id];
-
-        for (uint i = 0; i < contactIds.length; i++) {
-            if (contactIds[i] == _id) {
-                contactIds[i] = contactIds[contactIds.length - 1];
-                contactIds.pop();
-                break;
-            }
-        }
     }
 
-    function getContact(uint _id)
-        public
-        view
-        returns (
-            uint id,
-            string memory firstName,
-            string memory lastName,
-            uint[] memory phoneNumbers
-        )
-    {
+    function getContact(uint _id) public view returns (Contact memory) {
         if (contacts[_id].id == 0) {
             revert ContactNotFound(_id);
         }
-
-        Contact memory contact = contacts[_id];
-        return (contact.id, contact.firstName, contact.lastName, contact.phoneNumbers);
+        return contacts[_id];
     }
 
     function getAllContacts() public view returns (Contact[] memory) {
-        Contact[] memory allContacts = new Contact[](contactIds.length);
+        uint count = 0;
+        for (uint i = 1; i < nextId; i++) {
+            if (contacts[i].id != 0) {
+                count++;
+            }
+        }
 
-        for (uint i = 0; i < contactIds.length; i++) {
-            allContacts[i] = contacts[contactIds[i]];
+        Contact[] memory allContacts = new Contact[](count);
+        uint index = 0;
+        for (uint i = 1; i < nextId; i++) {
+            if (contacts[i].id != 0) {
+                allContacts[index] = contacts[i];
+                index++;
+            }
         }
 
         return allContacts;
