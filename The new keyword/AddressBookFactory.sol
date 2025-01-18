8efc19b574863eaf7b8d5512a9a6d929469ae070
armansoheili <113059160+armansoheili@users.noreply.github.com>
2025-01-19 08:27:43 +1200

    Update AddressBookFactory.sol

--- The new keyword/AddressBookFactory.sol
+++ The new keyword/AddressBookFactory.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
@@ -8,9 +7,15 @@ contract AddressBookFactory {
     event AddressBookDeployed(address indexed owner, address indexed addressBook);
 
     function deploy() public returns (address) {
+        // Deploy the AddressBook contract
         AddressBook newAddressBook = new AddressBook();
+
+        // Transfer ownership of the AddressBook to the deployer
         newAddressBook.transferOwnership(msg.sender);
+
+        // Emit an event with the owner and deployed contract address
         emit AddressBookDeployed(msg.sender, address(newAddressBook));
+
         return address(newAddressBook);
     }
 }
